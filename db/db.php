c271740edc1d83d08f138b9211ec9bad833a0295
Avinash K <todisturbedlord@gmail.com>
2026-02-26 15:53:00 +0530

    render DB Changes

--- db/db.php
+++ db/db.php
@@ -1,9 +1,9 @@
 <?php
 $url = $_ENV["DATABASE_URL"] ?? getenv("DATABASE_URL");
 $db = parse_url($url);
-
+$port = $db['port'] ?? 5432;
 $pdo = new PDO(
-    "pgsql:host={$db['host']};port={$db['port']};dbname=" . ltrim($db["path"], "/"),
+    "pgsql:host={$db['host']};port={$port};dbname=" . ltrim($db["path"], "/"),
     $db["user"],
     $db["pass"],
     [PDO::ATTR_ERRMODE =>
