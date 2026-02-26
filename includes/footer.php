00c83b18039748fc1e453a23b8a183ab91c7214c
Avinash K <todisturbedlord@gmail.com>
2026-02-26 23:59:23 +0530

    fixed whatsapp bug

--- includes/footer.php
+++ includes/footer.php
@@ -103,7 +103,7 @@
     Rights Reserved.
   </div>
   <a
-    href="https://wa.me/919361795050"
+    href="https://api.whatsapp.com/send?phone=919361795050"
     class="fixed bottom-6 right-6 bg-green-500 text-white p-4 rounded-full shadow-lg hover:scale-110 transition"
   >
     💬
