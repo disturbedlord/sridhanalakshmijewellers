7aaffdc41505f64a93b06874c569c82630608a76
Avinash K <todisturbedlord@gmail.com>
2026-03-11 17:47:14 +0530

    Added Node Start Script

--- pinoLogger.js
+++ pinoLogger.js
@@ -1,8 +1,11 @@
 import pino from "pino";
+import pretty from "pino-pretty";
 
 // Pino logger
-export const logger = pino({
-  level: "info", // change to 'debug' for more verbose logs
-  base: { service: "dljs_backend" },
-  timestamp: pino.stdTimeFunctions.isoTime,
-});
+export const logger = pino(
+  pretty({
+    level: "info", // change to 'debug' for more verbose logs
+    base: { service: "dljs_backend" },
+    timestamp: pino.stdTimeFunctions.isoTime,
+  }),
+);
